37d708076dee7a29c6d4db885b9dedb6174df749
Marko Melle <markmelle1@gmail.com>
2022-12-11 22:05:22 +0100

    add Version 2.1

--- src/js/team.js
+++ src/js/team.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-plusplus */
 export default class Team {
-  #counter = 0;
-
-  #current = 0;
+  constructor() {
+    this.counter = 0;
+    this.current = 0;
+  }
 
   addChar(char) {
-    this[`char${this.#counter++}`] = char;
+    this[`char${this.counter++}`] = char;
   }
 
   * [Symbol.iterator]() {
     const last = 2;
-    while (this.#current < last) yield this[`char${this.#current++}`];
+    while (this.current < last) yield this[`char${this.current++}`];
   }
 
   next() {
